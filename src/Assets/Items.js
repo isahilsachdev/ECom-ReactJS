6cd50762f870339775c8fb7da55beb4a999ca589
Sahil Sachdev <sahil.sachdev@inai.io>
2023-02-06 00:50:09 +0530

    mock data

---
+++ src/Assets/Items.js
@@ -0,0 +1,254 @@
+export const items = [
+  {
+    "item_id": 32,
+    "name": "POLO",
+    "category": "T-shirt",
+    "price": "849",
+    "discount": "15",
+    "fitting": "Regular Fit",
+    "color": [
+      "black",
+      "red",
+      "green"
+    ],
+    "sizes": [
+      "s",
+      "m",
+      "l"
+    ],
+    "image": [
+      "https://lmsin.net/cdn-cgi/image/h=1200,w=1200,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000009811686-Black-Black-1000009811686_01-2100.jpg"
+    ]
+  },
+  {
+    "item_id": 33,
+    "name": "LOUIS PHILIPPE",
+    "category": "Casual Shirt",
+    "price": "2799",
+    "discount": "30",
+    "fitting": "Regular Fit",
+    "color": [
+      "white",
+      "red",
+      "green"
+    ],
+    "sizes": [
+      "s",
+      "m",
+      "l"
+    ],
+    "image": [
+      "https://lmsin.net/cdn-cgi/image/h=1200,w=1200,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010698237-White-1000010698237_01-2100.jpg"
+    ]
+  },
+  {
+    "item_id": 34,
+    "name": "UCLA",
+    "category": "T-Shirt",
+    "price": "899",
+    "discount": "25",
+    "fitting": "Regular Fit",
+    "color": [
+      "green",
+      "red",
+      "green"
+    ],
+    "sizes": [
+      "s",
+      "m",
+      "l"
+    ],
+    "image": [
+      "https://lmsin.net/cdn-cgi/image/h=1200,w=1200,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000005978744-30119-Green-Dark-Green-1000005978744-30119_01-2100.jpg"
+    ]
+  },
+  {
+    "item_id": 35,
+    "name": "POLO",
+    "category": "T-shirt",
+    "price": "1199",
+    "discount": "40",
+    "fitting": "Regular Fit",
+    "color": [
+      "orange",
+      "red",
+      "green"
+    ],
+    "sizes": [
+      "s",
+      "m",
+      "l"
+    ],
+    "image": [
+      "https://lmsin.net/cdn-cgi/image/h=1200,w=1200,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010314170-Orange-Orange-1000010314170_01-2100.jpg"
+    ]
+  },
+  {
+    "item_id": 36,
+    "name": "WRANGLER",
+    "category": "Jeans",
+    "price": "2096",
+    "discount": "30",
+    "fitting": "Regular Fit",
+    "color": [
+      "white",
+      "red",
+      "green"
+    ],
+    "sizes": [
+      "s",
+      "m",
+      "l"
+    ],
+    "image": [
+      "https://lmsin.net/cdn-cgi/image/h=1200,w=1200,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000009038438-Blue-SuperStone-1000009038438_01-2100.jpg"
+    ]
+  },
+  {
+    "item_id": 37,
+    "name": "LOUIS PHILIPPE ",
+    "category": "Trousers",
+    "price": "1799",
+    "discount": "10",
+    "fitting": "Regular Fit",
+    "color": [
+      "blue",
+      "red",
+      "green"
+    ],
+    "sizes": [
+      "s",
+      "m",
+      "l"
+    ],
+    "image": [
+      "https://lmsin.net/cdn-cgi/image/h=1200,w=1200,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010148219-Blue-Blue-1000010148219_01-2100.jpg"
+    ]
+  },
+  {
+    "item_id": 38,
+    "name": "CODE",
+    "category": "Trousers",
+    "price": "1104",
+    "discount": "15",
+    "fitting": "Regular Fit",
+    "color": [
+      "yellow",
+      "red",
+      "green"
+    ],
+    "sizes": [
+      "s",
+      "m",
+      "l"
+    ],
+    "image": [
+      "https://lmsin.net/cdn-cgi/image/h=1200,w=1200,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000009801789-Beige-LightKhaki-1000009801789_01-2100.jpg"
+    ]
+  },
+  {
+    "item_id": 39,
+    "name": "KAPPA",
+    "category": "Track Pants",
+    "price": "849",
+    "discount": "15",
+    "fitting": "Regular Fit",
+    "color": [
+      "black",
+      "red",
+      "green"
+    ],
+    "sizes": [
+      "s",
+      "m",
+      "l"
+    ],
+    "image": [
+      "https://lmsin.net/cdn-cgi/image/h=1200,w=1200,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000009813654-Blue-Navy-1000009813654_01-2100.jpg"
+    ]
+  },
+  {
+    "item_id": 40,
+    "name": "KAPPA ",
+    "category": "Shorts",
+    "price": "699",
+    "discount": "30",
+    "fitting": "Regular Fit",
+    "color": [
+      "white",
+      "red",
+      "green"
+    ],
+    "sizes": [
+      "s",
+      "m",
+      "l"
+    ],
+    "image": [
+      "https://lmsin.net/cdn-cgi/image/h=1200,w=1200,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000009812654-Var7-White-White-1000009812654-Var7_01-2100.jpg"
+    ]
+  },
+  {
+    "item_id": 41,
+    "name": "KAPPA",
+    "category": "Track Pants",
+    "price": "849",
+    "discount": "30",
+    "fitting": "Regular Fit",
+    "color": [
+      "green",
+      "red",
+      "green"
+    ],
+    "sizes": [
+      "s",
+      "m",
+      "l"
+    ],
+    "image": [
+      "https://lmsin.net/cdn-cgi/image/h=1200,w=1200,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000009813654-Var9-Green-Olive-1000009813654-Var9_01-2100.jpg"
+    ]
+  },
+  {
+    "item_id": 42,
+    "name": "PUMA",
+    "category": "Track Pants",
+    "price": "2499",
+    "discount": "20",
+    "fitting": "Regular Fit",
+    "color": [
+      "blue",
+      "red",
+      "green"
+    ],
+    "sizes": [
+      "s",
+      "m",
+      "l"
+    ],
+    "image": [
+      "https://lmsin.net/cdn-cgi/image/h=1200,w=1200,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010653054-Blue-1000010653054_01-2100.jpg"
+    ]
+  },
+  {
+    "item_id": 43,
+    "name": "KAPPA",
+    "category": "Shorts",
+    "price": "849",
+    "discount": "15",
+    "fitting": "Regular Fit",
+    "color": [
+      "white",
+      "red",
+      "green"
+    ],
+    "sizes": [
+      "s",
+      "m",
+      "l"
+    ],
+    "image": [
+      "https://lmsin.net/cdn-cgi/image/h=1200,w=1200,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010194961-White-White-1000010194961_01-2100.jpg"
+    ]
+  }
+]
