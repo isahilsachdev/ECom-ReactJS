ad20db7d576ea72fe9b25cd60c42ef606fb6cc30
Sahil Sachdev <sahil.sachdev@inai.io>
2023-02-06 16:03:34 +0530

    updated base URL

--- src/Assets/axiosApiCall.js
+++ src/Assets/axiosApiCall.js
@@ -3,7 +3,7 @@ const baseURL = "https://ecommerce-nodejs-cqng69ehx-isahilsachdev.vercel.app/"
 
 // util to make axios api calls
 function axiosApiCall(url,method,data){
-    console.log(`Api call success on url ${process.env.REACT_APP_BASE_URL}${url} for ${method} method with ${data}.`);
+    console.log(`Api call success on url ${baseURL}${url} for ${method} method with ${data}.`);
     return axios[method.toLowerCase()]
     (`${baseURL}${url}`, data);
 }
